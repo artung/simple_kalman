31428645ac3abc6760dce47051f1f6daea63c757
Auralius Manurung <manurung.auralius@gmail.com>
2015-04-18 09:45:22 +0200

    Example 3 is working.

--- main3.cpp
+++ main3.cpp
@@ -34,21 +34,29 @@ int main(int argc, char** argv)
     Q = Q.eye();
       
     R = 10 * R.eye();
+    
+    colvec x0(4);
+    x0 << 10 << 10 << 1 << 0;
+    
+    mat P0(4,4);
+    P0 = 10 * P0.eye();
       
     SimpleKalman kalman;
     kalman.InitSystem(A, B, H, Q, R);
+    kalman.InitSystemState(x0);
+    kalman.InitStateCovariance(P0);
     
     colvec z(4);
     colvec x(4), x_m(4);
     colvec u(1);
     
     // No inputs, system subjects only to random perturbation
-    u(0) = 0; 
+    u = u.zeros(); 
   
-    for (int i = 0; i < 30 ; i ++) {  
+    for (int i = 0; i < 100 ; i ++) {  
       kalman.Kalmanf(x, x_m, u);
-      log_file << i << '\t' << x(0) << '\t' << x(1) << '\t' << x_m(0) << '\t' << x_m(1) << '\t' << u(0) << '\n';
-      
+      log_file << x(0) << '\t' << x(1) << '\t' << x(2) << '\t' << x(3) << '\t' 
+               << x_m(0) << '\t' << x_m(1) << '\t' << x_m(2) << '\t' << x_m(3) << '\t' << '\n';
     }
     
     log_file.close();
