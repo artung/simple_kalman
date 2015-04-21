f9d6a5bd068fa5e093e4898ca3812f31d4ed2472
Auralius Manurung <manurung.auralius@gmail.com>
2015-04-21 17:23:25 +0200

    dded main4.cpp, an example for the Extended Kalman Filter.

---
+++ main4.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+
+#include "ekf.h"
+
+class MyEKF: public EKF
+{
+public:
+  MyEKF(int n_states, int n_outputs, const mat& Q, const mat& R)
+    :EKF::EKF(n_states, n_outputs, Q, R)
+  {
+  }
+  
+  virtual colvec f(const colvec& x, const colvec& u, const int k) {
+    f_(0) = sin(x(1) * k);
+    f_(1) = sin(x(1));
+    return f_;
+  }
+  
+  virtual colvec h(const colvec& x, const colvec& u, const int k) {
+    h_(0) = x(0);
+    h_(1) = x(1);
+    return h_;
+  }
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char** argv)
+{
+  /* 
+   * Log the result into a tab delimitted file, later we can open 
+   * it with Matlab. Use: plot_data4.m to plot the results.
+   */
+  ofstream log_file;
+  log_file.open("log_file.txt");
+  
+  int n_states = 2;
+  int n_outputs = 2;
+  mat Q(2, 2);
+  mat R(2, 2);
+  
+  Q << 0.001 << 0    << endr
+    << 0     << 0    << endr;
+    
+  R << 0.1   << 0    << endr
+    <<   0   << 0.01 << endr;
+  
+  colvec x0(3);
+  x0 << 0 << 3 * M_PI / 500;
+  
+  colvec x, x_m;
+  colvec u;
+  
+  MyEKF myekf(n_states, n_outputs, Q, R);
+  myekf.InitSystemState(x0);
+  
+  for (int k = 0; k < 1000; k ++) {
+    myekf.EKalmanf(x, x_m, u, k);
+    log_file << x(0) << '\t' << x(1) << '\t' << x_m(0) << '\t' << x_m(1) << '\n'; 
+  }
+  
+  log_file.close();
+  
+  return 0;
+}
